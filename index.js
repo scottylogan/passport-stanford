b85fb5d25af01d0cbc5253a7dd657596c4eb983e
Scotty Logan <swl@stanford.edu>
2017-10-11 10:59:40 -0700

    bumped acceptedClockSkewMs to 60 seconds

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function suSAML (samlConfig) {
     protocol: 'https://',
     signatureAlgorithm: 'sha256',
     identifierFormat: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-    acceptedClockSkewMs: 2000,
+    acceptedClockSkewMs: 60000,
     attributeConsumingServiceIndex: false,
     disableRequestedAuthnContext: true,
     forceAuthn: false,
